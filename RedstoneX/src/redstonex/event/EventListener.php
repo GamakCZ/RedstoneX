9a6f19311a2aae898b098a7e619383adcb433246
GamakCZ <gamak.mcpe@gmail.com>
2017-11-11 12:37:25 +0100

    fix debug

--- RedstoneX/src/redstonex/event/EventListener.php
+++ RedstoneX/src/redstonex/event/EventListener.php
@@ -23,8 +23,9 @@ class EventListener implements Listener {
     public function onPlace(BlockPlaceEvent $event) {
         switch ($event->getBlock()->getId()) {
             case Block::REDSTONE_TORCH:
+                $event->setCancelled(true);
                 $event->getBlock()->getLevel()->setBlock($event->getBlock()->asVector3(), new RedstoneTorch(0), false, false);
-                if($event->getBlock() instanceof Redstone) {
+                if($event->getBlock() instanceof RedstoneTorch) {
                     RedstoneX::getInstance()->getLogger()->info("Placing block (Redstone Torch) (redstonex block)");
                 }
                 else {
@@ -33,12 +34,11 @@ class EventListener implements Listener {
                         RedstoneX::getInstance()->getLogger()->info("Placed block (Redstone Torch) (pmmp block)");
                     }
                 }
-                $event->setCancelled(true);
                 return;
             case Block::REDSTONE_WIRE:
                 $event->getBlock()->getLevel()->setBlock($event->getBlock()->asVector3(), new Redstone(RedstoneX::REDSTONE_WIRE, 0, "Redstone Wire", RedstoneX::REDSTONE_ITEM));
                 $event->setCancelled(true);
-                if($event->getBlock() instanceof RedstoneTorch) {
+                if($event->getBlock() instanceof Redstone) {
                     RedstoneX::getInstance()->getLogger()->info("Placing block (Redstone Wire) (redstonex block)");
                 }
                 else {
